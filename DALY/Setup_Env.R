38f49d8d5bf6d6906a3c2e817ab4d1337b4f1c1e
ArneNoppe <160399278+ArneNoppe@users.noreply.github.com>
2026-03-24 16:18:27 +0100

    Update Setup_Env.R - added table with DWs

--- DALY/Setup_Env.R
+++ DALY/Setup_Env.R
@@ -11,3 +11,10 @@ library(tidyr)
 library(devtools)
 devtools::install_github("sciensanogit/BeBOD")
 
+# Define disability weights by health state - ordered from "mild" to "severe"
+weightsSMA <- data.frame(MOT = c(0.010, 0.061, 0.268, NA, NA, NA),
+                  PULM = c(0.025, 0.284, 0.418, NA, NA, NA),
+                  MSK = c(0.079, 0.112, 0.028, 0.117, 0.317, 0.581),
+                  SCOL = c(0.372, NA, NA, NA, NA, NA),
+                  SPEECH  = c(0.051, NA, NA, NA, NA, NA),
+                  FEED = c(0.163, NA, NA, NA, NA, NA))
